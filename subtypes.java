e4949b3cb048f9be371a4f7a2af5949b07879542
mwm13-beep <mwm13ca@hotmail.com>
2023-11-30 14:36:37 -0600

    Subtypes file update
    updating filtering of subtypes

--- subtypes.java
+++ subtypes.java
@@ -10,31 +10,41 @@ public class subtypes {
 
     public static void main(String[] args){
         try{
-            FileWriter writer = new FileWriter("attributes.json");
+            FileWriter writer = new FileWriter("attributes.txt");
             BufferedReader br = new BufferedReader(new FileReader(new File("restSubtypes.txt")));
             ArrayList<String> attributes = new ArrayList<String>();
 
             String[] splits;
+            String[] splitOnSpace;
             String line;
             String trimmed;
+            String restRemoved = "";
 
-            writer.write("{\n");
+            writer.write("[\n");
             int j = 0;
             while ((line = br.readLine()) != null){
                 splits = line.split(",");
-
                 for( int i = 0; i < splits.length; i++ ){
                     trimmed = splits[i].trim();
-                    if( !attributes.contains(trimmed) ){
-                        attributes.add(trimmed);
-                        writer.write("\"" + j + "\": \"" +  trimmed + "\",\n");
-                        j++;
+                    splitOnSpace = trimmed.split(" ");
+                    for( int k = 0; k < splitOnSpace.length; k++ ){        
+                        if( !splitOnSpace[k].equals("Restaurant") && !splitOnSpace[k].equals("restaurant") ){
+                            restRemoved += splitOnSpace[k] + " ";
+                        }
+                         
+                         
                     }
+                    if(!restRemoved.equals("") && !attributes.contains(restRemoved.trim())){
+                        attributes.add(restRemoved.trim());
+                        writer.write("\t{\"0\": \"" +  restRemoved.trim() + "\"},\n");
+                    }
+                    restRemoved = "";
+                    j++;
                 }
 
             }
             
-            writer.write("}");
+            writer.write("]");
             writer.close();
 
         } catch (FileNotFoundException e) {
